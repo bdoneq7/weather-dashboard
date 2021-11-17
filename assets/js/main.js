a88a205143216e10e70e6f50f2fca138fa22b471
Brian Done <briguy2078@gmail.com>
2021-11-17 16:11:48 -0500

    added api fetch call to open weather map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 var getUserCity = function() {
     console.log("function was called");
+
+    // Open Weather Map api url
+    // reference 6.1.6
+    var response = fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8ae55ceca5827c1c46c002c58abf5277").then(function(response){
+        
+        response.json().then(function(data){
+            console.log(data);
+        })
+    })
+    console.log(response);
 };
 
 getUserCity();
