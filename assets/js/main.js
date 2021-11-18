4235368751b002a00fb041bb3ddc24ac7aadc21f
Brian Done <briguy2078@gmail.com>
2021-11-18 15:18:49 -0500

    added id's for html elements and city search function to retrieve weather

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,33 @@
-var getUserCity = function() {
-    console.log("function was called");
+var citysearches = [];
 
-    // Open Weather Map api url
-    // reference 6.1.6
-    var response = fetch("api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8ae55ceca5827c1c46c002c58abf5277").then(function(response){
-        
-        response.json().then(function(data){
-            console.log(data);
-        })
-    })
-    console.log(response);
+var citySearchEl=document.querySelector("#city-search");
+var cityEl=document.querySelector("#city");
+var currentWeatherEl=document.querySelector("#current-weather");
+var citySearchEl = document.querySelector("#city-search");
+
+
+var citySearchForm = function(event){
+    
+    var city = cityEl.value.trim();
+    if(city){
+        retrieveCityWeather(city);
+        citysearches.unshift({city});
+        cityEl.value = "";
+    } else{
+        console.log("redirect to homepage");
+    }
+    localStorage.setItem("citysearches", JSON.stringify(citysearches));
+    
 };
 
-getUserCity();
+var retrieveCityWeather = function(city){
+    
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=23490a99fb08838927ee6b3f63514da6"
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            displayWeather(data, city);
+        });
+    });
+};
