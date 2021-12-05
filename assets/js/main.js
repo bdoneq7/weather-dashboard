27274e834ec052e06d8da8e453c1910828a9c617
Brian Done <briguy2078@gmail.com>
2021-12-05 14:57:57 -0500

    updated html structure and restarted js function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,8 @@
-var citysearches = [];
-
-var citySearchEl=document.querySelector("#city-search");
-var cityEl=document.querySelector("#city");
-var currentWeatherEl=document.querySelector("#current-weather");
-var citySearchEl = document.querySelector("#city-search");
-
-
-var citySearchForm = function(event){
-    
-    var city = cityEl.value.trim();
-    if(city){
-        retrieveCityWeather(city);
-        citysearches.unshift({city});
-        cityEl.value = "";
-    } else{
-        console.log("redirect to homepage");
-    }
-    localStorage.setItem("citysearches", JSON.stringify(citysearches));
-    
+function weatherDashboard() {
+    var cityInputEl = document.getElementById("city-search");
+    var searchButtonEl = document.getElementById("button-search");
+    var clearHistoryEl = document.getElementById("history-clear");
+    var cityNameEl = document.getElementById("city-name");
 };
 
-var retrieveCityWeather = function(city){
-    
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=23490a99fb08838927ee6b3f63514da6"
-
-    fetch(apiURL)
-    .then(function(response){
-        response.json().then(function(data){
-            displayWeather(data, city);
-        });
-    });
-};
+weatherDashboard ();
